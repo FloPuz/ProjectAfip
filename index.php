ea518ced48f423046d3011fd3d0d5069ed0a5767
FloPuz <fpuzenat1@gmail.com>
2021-09-08 15:51:34 +0200

    Deuxième commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 
 function ServiceHandler() {
     $data = new stdClass();
-    $data->Output = "Hello World!";
+    $data->Output = "Hello l'afip c'est le projet de puz!";
 
     return var_dump($data);
 };
